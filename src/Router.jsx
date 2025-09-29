0cee314f05cd9d324e4e9048c541cbde63382c05
kassushale1980 <kassushale@gmail.com>
2025-09-29 19:58:07 -0400

    deploying with netlify base name added

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,51 @@
+// import React from 'react'
+// import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+// import Landing from './pages/Landing/Landing'
+// import Auth from "./pages/Auth/Auth";
+// import Payment from './pages/Payment/Payment'
+// import Orders from './pages/Orders/Orders'
+// import Cart from './pages/Cart/Cart'
+// import Results from './pages/Results/Results'
+// import ProductDetail from './pages/ProductDetail/ProductDetail'
+// import {Elements} from '@stripe/react-stripe-js';
+// import {loadStripe} from '@stripe/stripe-js';
+// import ProtectRoute from "./Components/ProtectedRoute/ProtectedRoute"
+
+// const stripePromise = loadStripe('pk_test_51SB2Y88Rswsty7NihCq4hsc1hqQlArRwOhXvibHN4NfJsBUQcxg8qBSP4wGS52fuyQUnIQS8AW75zDhASAcjGvbU00iySrAC6j'
+// );
+// function Routing() {
+//   return (
+//     <Router>
+//         <Routes>
+//             <Route path="/" element={<Landing/>}/>
+//            <Route path="/auth" element={<Auth />} />
+//             <Route path="/checkout" element={
+//               <ProtectRoute msg={"you must login to pay"}
+//               redirect={"/checkout"}>
+//             <Elements stripe={stripePromise}>
+//                  <Payment />
+//               </Elements>
+//               </ProtectRoute>
+             
+//               } />
+//             <Route path="/orders" element={
+//               <ProtectRoute msg={"you must login to access your order"} redirect={"/orders"}>
+//                 <Orders />
+//               </ProtectRoute>
+//             } />
+              
+//              <Route path="/category/:categoryName" element={<Results/>}/>
+//              <Route path="/products/:productId" element ={<ProductDetail/>} />
+//              <Route path="/cart" element={<Cart />} />
+//         </Routes>
+//    </Router>
+//   )
+// }
+
+// export default Routing;
+
 import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from './pages/Landing/Landing'
 import Auth from "./pages/Auth/Auth";
 import Payment from './pages/Payment/Payment'
@@ -7,43 +53,45 @@ import Orders from './pages/Orders/Orders'
 import Cart from './pages/Cart/Cart'
 import Results from './pages/Results/Results'
 import ProductDetail from './pages/ProductDetail/ProductDetail'
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import ProtectRoute from "./Components/ProtectedRoute/ProtectedRoute"
 
-const stripePromise = loadStripe('pk_test_51SB2Y88Rswsty7NihCq4hsc1hqQlArRwOhXvibHN4NfJsBUQcxg8qBSP4wGS52fuyQUnIQS8AW75zDhASAcjGvbU00iySrAC6j'
-);
+const stripePromise = loadStripe('pk_test_51SB2Y88Rswsty7NihCq4hsc1hqQlArRwOhXvibHN4NfJsBUQcxg8qBSP4wGS52fuyQUnIQS8AW75zDhASAcjGvbU00iySrAC6j');
+
 function Routing() {
   return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Landing/>}/>
-           <Route path="/auth" element={<Auth />} />
-            <Route path="/checkout" element={
-              <ProtectRoute msg={"you must login to pay"}
-              redirect={"/checkout"}>
-            <Elements stripe={stripePromise}>
-                 <Payment />
+    <Router basename="/Amazon-clone">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/checkout"
+          element={
+            <ProtectRoute msg={"you must login to pay"} redirect={"/checkout"}>
+              <Elements stripe={stripePromise}>
+                <Payment />
               </Elements>
-              </ProtectRoute>
-             
-              } />
-            <Route path="/orders" element={
-              <ProtectRoute msg={"you must login to access your order"} redirect={"/orders"}>
-                <Orders />
-              </ProtectRoute>
-            } />
-              
-             <Route path="/category/:categoryName" element={<Results/>}/>
-             <Route path="/products/:productId" element ={<ProductDetail/>} />
-             <Route path="/cart" element={<Cart />} />
-        </Routes>
-   </Router>
+            </ProtectRoute>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <ProtectRoute msg={"you must login to access your order"} redirect={"/orders"}>
+              <Orders />
+            </ProtectRoute>
+          }
+        />
+        <Route path="/category/:categoryName" element={<Results />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   )
 }
 
 export default Routing;
-
 
 
 
